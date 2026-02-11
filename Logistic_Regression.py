b2f0bffe811b945358e5b9dc11c385f40387fe27
Dino-bit00 <arya.yusuffadilah@gmail.com>
2026-02-11 21:00:51 +0700

    Created model logistic regression in file Logistic_Regression.py

--- Logistic_Regression.py
+++ Logistic_Regression.py
@@ -3,7 +3,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import joblib
 from sklearn.model_selection import train_test_split
-from sklearn.logistic_regression import LogisticRegression
+from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
@@ -16,3 +16,20 @@ df = df.dropna(subset=['Clean Narasi'])
 
 X = df['Clean Narasi']
 y = df['hoax']
+
+# Pipleine
+pipeline = Pipeline([
+	('tfidf', TfidfVectorizer()),
+	('classifier', LogisticRegression())
+])
+
+# Split dataset
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Train model
+pipeline.fit(X_train, y_train)
+
+# Prediksi dan evaluasi
+y_pred = pipeline.predict(X_test)
+print(f"Accuracy: {accuracy_score(y_test, y_pred)}")
+print(classification_report(y_test, y_pred))
